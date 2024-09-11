83526caf395f82d9588559c8702bf42786b89f6d
Lucas <115151663+Oceiroz@users.noreply.github.com>
2024-09-11 13:02:26 +0100

    Add files via upload

---
+++ Next/Next.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <stdexcept>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+template <typename T>
+T GetInput(string inputMessage);
+
+int main()
+{
+    int value = GetInput<int>("Please enter a number so it can be sorted");
+    string strValue = to_string(value);
+    sort(strValue.begin(), strValue.end(), greater<char>());
+    int sortedValue = stoi(strValue);
+    if (sortedValue == value)
+    {
+        cout << -1 << endl;
+    }
+    else
+    {
+        cout << sortedValue << endl;
+    }
+}
+template <typename T>
+T GetInput(string inputMessage)
+{
+    T userInput;
+    while (true)
+    {
+        cout << inputMessage << endl << endl;
+        string rawInput;
+        getline(cin, rawInput);
+        try
+        {
+            istringstream iss(rawInput);
+            iss >> userInput;
+            if (iss.fail() && iss.peek() != EOF)
+            {
+                throw logic_error("This is not the right data type");
+            }
+            break;
+        }
+        catch (const exception& e)
+        {
+            cerr << e.what() << endl << endl;
+        }
+    }
+    return userInput;
+}
